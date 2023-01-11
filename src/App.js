53bd7c7e588e67de6728abc88e7bc283d237323b
Денис Зубрицкий <fort_zd@mail.ru>
2023-01-11 13:50:45 +0700

    урок 20, первая часть задания по rrd5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  NavLink,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Layout</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/users" component={UsersLayout} />
+          <Route path="/" component={MainPage} />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
+}
+
+function MainPage() {
+  const data = useRouteMatch();
+  console.log(data);
+  return <h1>Main</h1>;
+}
+
+function UsersLayout() {
+  const { path } = useRouteMatch();
+  return (
+    <div>
+      <h1>Users Layout</h1>
+      <Switch>
+        <Route path={path + "/:userId/profile"} component={UserProfilePage} />
+        <Route path={path + "/:userId/edit"} component={EditUserPage} />
+        <Route path={path} exact component={UserListPage} />
+        <Redirect from={path + "/:userId"} to={path + "/:userId/profile"} />
+      </Switch>
+    </div>
+  );
+}
+
+function UserListPage() {
+  const { path } = useRouteMatch();
+  return (
+    <div>
+      <h1>User List Page</h1>
+      <ul>
+        {new Array(5).fill("").map((_, index) => (
+          <li key={"user_list_component_" + index}>
+            <Link to={`${path}/${index}`}>User {index}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function UserProfilePage() {
+  const { userId } = useParams();
+  return (
+    <div>
+      <h1>User Page</h1>
+      <p>userId: {userId}</p>
+    </div>
+  );
+}
+
+function EditUserPage() {
+  return <h1>Edit User Page</h1>;
 }
 
 export default App;
